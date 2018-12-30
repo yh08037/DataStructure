de45c2145444ca3c91ba3eb6eeb41bb9a84c551c
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2018-12-30 15:17:38 +0900

    Create 2-노드 추가.c

---
+++ 1. List/1-1. LinkedList/2-노드 추가.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+typedef struct tagNode{
+    int Data;
+    struct tagNode* NextNode;
+}Node;
+
+Node* SLL_CreateNode(int NewData){
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+
+    NewNode->Data = NewData;
+    NewNode->NextNode = NULL;
+
+    return NewNode;
+}
+
+
+void SLL_DestroyNode(Node* Node){
+    free(Node);
+}
+
+
+void SLL_AppendNode(Node** Head, Node* NewNode){
+    if(*Head == NULL){
+        *Head = NewNode;
+    }
+    else{
+        Node* Tail = *Head;
+        while(Tail->NextNode != NULL){
+            Tail = Tail->NextNode;
+        }
+        Tail->NextNode = NewNode;
+    }
+}
+
+
+
+/*AppendNode() 사용 예시*/
+
+int main(void){
+
+    Node* List = NULL;
+    Node* NewNode = NULL;
+
+    NewNode = SLL_CreateNode(117);
+    SLL_AppendNode(&List, NewNode);
+
+    NewNode = SLL_CreateNode(199);
+    SLL_AppendNode(&List, NewNode);
+
+    printf("%d ", List->Data);
+    printf("%d ", List->NextNode->Data);
+
+    return 0;
+}
+
+
+

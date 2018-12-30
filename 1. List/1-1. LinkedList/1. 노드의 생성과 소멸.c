32345d996bc5a8f6862f0afaf3169b80002c5458
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2018-12-30 15:17:00 +0900

    Create 1. 노드의 생성과 소멸.c

---
+++ 1. List/1-1. LinkedList/1. 노드의 생성과 소멸.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct tagNode{
+    int Data;
+    struct tagNode* NextNode;
+}Node;
+
+Node* SLL_CreateNode(int NewData){
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+
+    NewNode->Data = NewData;
+    NewNode->NextNode = NULL;
+
+    return NewNode;
+}
+
+
+void SLL_DestroyNode(Node* Node){
+    free(Node);
+}
+

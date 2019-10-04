cfba922bf52dc95739ca0b2e4c1a8eb0b5352827
Dohun Kim <yh08037@gmail.com>
2019-10-04 13:14:21 +0900

    change variable name

--- hw1_refresh/decimal_to_binary.c
+++ hw1_refresh/decimal_to_binary.c
@@ -1,30 +1,35 @@
-// #include "arr_stack.h"
-#include "ll_stack.h"
+#include "arr_stack.h"
+// #include "ll_stack.h"
 #include <stdio.h>
 
 int main() {
     int decimal_number;
     int data;
 
+    Stack* stack;
+
     printf("Enter an integer : ");
     scanf("%d", &decimal_number);
 
-    Stack* pStack = CreateStack(10);
+    if ( !(stack = CreateStack(100)) ) {
+      printf("Memory allocation of stack failed. Program terminates.\n");
+      return -1;
+    }
 
     while (decimal_number != 0) {
         data = decimal_number % 2;
-        if (IsFullStack(pStack)) {
+        if (IsFullStack(stack)) {
             printf("Input number is larger than size of stack. Program terminates.\n");
-            return -1;
+            return -2;
         }
-        Push(pStack, data);
+        Push(stack, data);
         decimal_number /= 2;
     }
 
     printf("The binary number is : ");
 
-    while (!IsEmptyStack(pStack)) {
-        data = Pop(pStack);
+    while ( !IsEmptyStack(stack) ) {
+        data = Pop(stack);
         printf("%d", data);
     }
     printf("\n");

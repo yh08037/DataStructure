4ae478593c5914d587d1c985e179301a1c8c944c
Dohun Kim <yh08037@gmail.com>
2019-11-03 21:44:25 +0900

    refactored complete_bt.c

--- hw4_complete_bt/complete_bt.c
+++ hw4_complete_bt/complete_bt.c
@@ -32,36 +32,36 @@ int get_left_child(Tree* bt, int index) {
 
 // Preorder traverasal : 전위순회
 void preorder(Tree* bt, int index) {
-   if (index > 0 && bt->tree[index]!='\0') {
-      printf("%c", bt->tree[index]);
-      preorder(bt, get_left_child(bt, index));
-      preorder(bt, get_right_child(bt, index));
-   }
+  if (index <= 0 || bt->tree[index] == '\0') return;
+
+  printf("%c", bt->tree[index]);
+  preorder(bt, get_left_child(bt, index));
+  preorder(bt, get_right_child(bt, index));
 }
 
 
 // Inorder traversal : 중위순회
 void inorder(Tree* bt, int index) {
-   if (index > 0 && bt->tree[index]!='\0') {
-      inorder(bt, get_left_child(bt, index));
-      printf("%c", bt->tree[index]);
-      inorder(bt, get_right_child(bt, index));
-   }
+  if (index <= 0 || bt->tree[index] == '\0') return;
+
+  inorder(bt, get_left_child(bt, index));
+  printf("%c", bt->tree[index]);
+  inorder(bt, get_right_child(bt, index));
 }
 
 
 // Post traversal : 후위순회
 void postorder(Tree* bt, int index) {
-   if (index > 0 && bt->tree[index]!='\0') {
-      postorder(bt, get_left_child(bt, index));
-      postorder(bt, get_right_child(bt, index));
-      printf("%c", bt->tree[index]);
-   }
+  if (index <= 0 || bt->tree[index] == '\0') return;
+
+  postorder(bt, get_left_child(bt, index));
+  postorder(bt, get_right_child(bt, index));
+  printf("%c", bt->tree[index]);
 }
 
 // Breadth-first traversal : 너비 우선 순회
 void breadth_first(Tree* bt, int index) {
-  while (index > 0 && bt->tree[index]!='\0')
+  while ( index > 0 && bt->tree[index] != '\0' )
     printf("%c", bt->tree[index++]);
 }
 
